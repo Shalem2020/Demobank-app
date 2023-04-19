5df4d2d30d39e9f9eb1000810af43891859a6f74
Shalem2020 <102743833+Shalem2020@users.noreply.github.com>
2023-04-19 19:49:12 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   return (
     <nav className = "w-full flex py-6 justify-between items-center Navbar">
-      <img src = {logo} alt = "Demoank"
+      <img src = {logo} alt = "HooBank"
       className = "w-[124px] h-[32px]" />
       <ul className ="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav,index) =>(
